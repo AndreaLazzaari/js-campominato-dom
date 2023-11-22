c07de01f5083e01d85dea74918a4341841dd35be
Andrea Lazzari <andrea.lazzari8@gmail.com>
2023-11-22 11:57:29 +0100

    creata funzione che permette di generare 16 numeri randomici tutti diversi

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
-// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-// Ogni cella ha un numero progressivo, da 1 a 100.
-// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-// Quando l'utente clicca su ogni cella, la cella cliccata 
-// si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
+// Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco.
+// Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe.
+// Attenzione:
+// nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+// In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati:
+// - abbiamo calpestato una bomba
+// - la cella si colora di rosso e la partita termina.
+// Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+// La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di 
+// numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+// Al termine della partita il software deve comunicare il punteggio, 
+// cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
 
-// generare funzione che crea un quadrato
-// creare una tabella 10x10
-// al click del bottone inserire in ogni cella un numero progressivo da 1 a 100
+
+// creare una funzione che generi 16 numeri casuali tutti diversi
+// i numeri generati vanno all'interno di un array
+// i numeri dentro all'array corrispondono alla rispettiva cella
+
+/**creato un array vuoto
+ * generato un numero casuale da 1 a 16
+ * condizione while per generare i 16 numeri tutti diversi
+ * @returns array con all'interno 16 numeri generati randomici tutti diversi
+ */
+function creazioneNumeriRandom() {
+  const numeriGenerati = [];
+  let num = Math.floor(Math.random() * 16) + 1;
+  while (numeriGenerati.includes(num)) {
+    num = Math.floor(Math.random() * 16) + 1;
+  }
+  return numeriGenerati;
+}
+
+
+
+
+
+
 
 let mainContainerElement = document.querySelector('div.main-container')
 let buttonElement = document.querySelector ('button');
